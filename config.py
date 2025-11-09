e6718eb843a204949442fff62f2871b1ad1cc518
KunalMohantaCoder <kunalmohanta19072008@gmail.com>
2025-11-09 19:01:00 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+email = "<your_email>"
+email_password = "<your_email_password>"
+wolframalpha_id = "<your_wolframalpha_id>"
+
+weather_api_key = "<your_api_key>"
+email = "<your_email>"
+email_password = "<your_email_password>"
+wolframalpha_id = "<your_wolframalpha_id>"
